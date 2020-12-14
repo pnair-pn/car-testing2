c5973b78667e00d3ba77af90097a40aab0cabf47
pnair-pn <74791544+pnair-pn@users.noreply.github.com>
2020-12-14 14:54:45 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# car-testing2
-car testing 2
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,42 @@
+var bullet, wall, thickness, weight, speed;
+var bulletRightEdge, wallLeftEdge;
+
+function setup() {
+  createCanvas(1600,400);
+  bullet = createSprite(50, 200, 50, 5);
+  wall = createSprite(1200,200,thickness,height/2);
+  speed=random(223,331);
+  weight=random(30,52);
+  thickness=random(22,83);
+}
+
+function draw() {
+  background(0);  
+
+  bullet.velocityX = speed;
+  bullet.shapeColor = color("white");
+
+  wall.shapeColor = color(80,80,80);
+
+  if (hasCollided(bullet,wall)){
+    bullet.velocityX = 0;
+    var damage=0.5 * weight * speed * speed/(thickness * thickness * thickness);
+  }
+  if (damage>10){
+    wall.shapeColor = color(255,0,0);
+  }
+  if (damage<10){
+    wall.shapeColor = color(0,255,0)
+  }
+
+  drawSprites();
+}
+
+function hasCollided(bullet, wall){
+  bullet=bullet.x + bullet.width;
+  wall=wall.x;
+  if (bullet>=wall){
+    return true
+  }
+  return false;
+}
